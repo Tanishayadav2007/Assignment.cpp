4031ab7df973dba9aca5d9d8629429fac043ae69
Tanisha Yadav <24csu214@ncuindia.edu>
2025-02-04 18:32:49 +0530

    Add files via upload

---
+++ prime.cpp
@@ -0,0 +1,26 @@
+//check whether number is prime or not
+#include<iostream>
+using namespace std;
+int main(){
+    int n,count=0;
+    cout<<"enter a number to check whether it is prime or not:"<<endl;
+    cin>>n;
+    int i;
+    if(n<=1){
+        cout<<"number is not prime";
+    }
+    else{
+        for(i=1;i<=n;i++){
+            if(n%i==0){
+                count++;
+            }
+        }
+        if(count>2){
+            cout<<"number is not prime"<<endl;
+        }
+        else
+        cout<<"number is prime";
+    }
+
+    return 0;
+}
